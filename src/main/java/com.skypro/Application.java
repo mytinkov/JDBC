a5a8b58fd2ace6852b1dc71be055504fd3f79bf2
tinkov <vasy499@mail.ru>
2023-03-25 13:58:44 +0300

    hibernate commit

--- src/main/java/com.skypro/Application.java
+++ src/main/java/com.skypro/Application.java
@@ -1,57 +1,34 @@
 package com.skypro;
 
 import java.sql.*;
+import java.util.List;
 
 public class Application {
-    public static void main(String[] args) throws SQLException {
-        final String user = "postgres";
-        final String password = "1786232t";
-        final String url = "jdbc:postgresql://localhost:5432/skypro2";
 
-        try (Connection connection = DriverManager.getConnection(url, user, password);
-             PreparedStatement statement = connection.prepareStatement(
-                     "SELECT first_name, last_name, gender, city_name " +
-                             "FROM employee INNER JOIN city " +
-                             "ON employee.city_id = city.city_id AND id = (?)"
-             )) {
-            statement.setInt(1, 3);
 
-            final ResultSet resultSet = statement.executeQuery();
+//    public static void main(String[] args) throws SQLException {
+//        final String user = "postgres";
+//        final String password = "1786232t";
+//        final String url = "jdbc:postgresql://localhost:5432/skypro2";
 
-            while (resultSet.next()) {
-                String firstName = "First name: " + resultSet.getString("first_name");
-                String lastName = "Last name: " + resultSet.getString("last_name");
-                String gender = "Gender: " + resultSet.getString("gender");
-                String city = "City: " + resultSet.getString("city_name");
+    public static void main(String[] args) {
+        EmployeeDAO employeeDAO = new EmployeeDAOImpl();
 
-                System.out.println(firstName);
-                System.out.println(lastName);
-                System.out.println(gender);
-                System.out.println(city);
-            }
+        Employee employee = new Employee("Vasy", "Vasya", "male", 30, 1);
+
+        employeeDAO.create(employee);
+
+        System.out.println(employeeDAO.readById(1));
+
+        List<Employee> employees = employeeDAO.readAll();
+
+        for (Employee entry : employees) {
+            System.out.println(entry);
         }
 
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
-            EmployeeDAO employeeDAO = new EmployeeDAOImpl(connection);
+        employee.setCity(2);
+        employeeDAO.update(employee);
 
-            City city = new City(1, null);
-            Employee employee = new Employee("Test", "Test", "male", 20, city);
-
-            System.out.println("CREATE ENTRY");
-            employeeDAO.create(employee);
-            System.out.println(employeeDAO.readAll());
-
-            System.out.println("UPDATE AGE");
-            employeeDAO.updateAgeById(1, 45);
-            System.out.println(employeeDAO.readById(3));
-
-            System.out.println("UPDATE CITY");
-            employeeDAO.updateCityById(1, new City(2, null));
-            System.out.println(employeeDAO.readById(3));
-
-            System.out.println("DELETE ENTRY");
-            employeeDAO.deleteById(2);
-            System.out.println(employeeDAO.readAll());
-        }
+        employeeDAO.delete(employee);
     }
 }
